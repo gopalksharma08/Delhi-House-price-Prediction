345e0480107d2f665ce35244c87db82d3bde5e5e
Gopal Sharma <gopalksharma08@gmail.com>
2020-08-15 20:31:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 import requests
 import numpy as np
 import pickle
-import xgboost
 
 app = Flask(__name__)
 model = pickle.load(open("houseprice.pkl", "rb"))
